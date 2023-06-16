f24779dbcc7dc23df11381aa3f6748a8af9f8f5f
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-06-16 16:44:32 -0600

    Update schema to required unique group names

--- prisma/migrations/20230616223714_init/migration.sql
+++ prisma/migrations/20230616223714_init/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `errors_groups` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `errors_groups_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
