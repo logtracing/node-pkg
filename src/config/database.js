302c1bd8e61718f0a3af883ce21e107757d7a7d3
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-07 16:56:25 -0600

    Migrate first model to Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'p4ssw0rd',
+    database: 'dev_logtracing',
+    host: '127.0.0.1',
+    port: 3308,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: 'p4ssw0rd',
+    database: 'dev_logtracing',
+    host: '127.0.0.1',
+    port: 3308,
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: 'p4ssw0rd',
+    database: 'dev_logtracing',
+    host: '127.0.0.1',
+    port: 3308,
+    dialect: 'mysql',
+  },
+};
---
+++ src/db/models/LogGroup.js
@@ -0,0 +1,28 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class LogGroup extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  LogGroup.init(
+    {
+      id: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: 'LogGroup',
+      tableName: 'logGroups',
+    }
+  );
+
+  return LogGroup;
+};
