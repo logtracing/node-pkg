96bcaaa668e91bd595556437d0d259f4309f982d
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-12 09:45:44 -0600

    Create 6 sample tests for the Logger class

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -1,10 +1,121 @@
-import { expect, describe, test } from '@jest/globals';
-import Logger from '../src/ExceptionLogger';
+import { expect, describe, test, afterEach, beforeEach } from '@jest/globals';
+import { Logger, LogType } from '../src/index';
+// @ts-ignore
+import { Log } from '../src/db/models/index';
 
-describe('Testing basic logging operations', () => {
-  test('Create a simple instance for a Testing flow', () => {
-    const logger: Logger = new Logger('Testing');
+describe('Testing Logger operations', () => {
+  let flow: string;
+  let group: string;
+  let content: string;
 
-    expect(logger.flow).toBe('Testing');
+  beforeEach(() => {
+    flow = `${Date.now()}`;
+    group = `${Date.now()}-group`;
+    content = `${Date.now()}-content`;
+  });
+
+  test('Write a simple trace log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.trace(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.TRACE);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  test('Write a simple debug log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.debug(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.DEBUG);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  test('Write a simple info log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.info(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.INFO);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  test('Write a simple warn log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.warn(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.WARN);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  test('Write a simple error log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.error(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.ERROR);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  test('Write a simple fatal log', async () => {
+    const logger: Logger = new Logger(flow);
+    await logger.fatal(content);
+
+    const savedLog = await Log.findOne({
+      where: {
+        flow,
+        content
+      }
+    });
+
+    expect(savedLog.level).toBe(LogType.FATAL);
+    expect(savedLog.flow).toBe(flow);
+    expect(savedLog.content).toBe(content);
+  });
+
+  afterEach(async () => {
+    await Log.destroy({
+      where: {
+        flow,
+        content,
+      }
+    })
   });
 });
