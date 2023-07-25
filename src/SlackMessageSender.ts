1120601dab949f3655955c5e04ce1d6bf301e4d0
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-25 10:16:57 -0600

    Add method to get a basic template for messages in Slack

--- src/SlackMessageSender.ts
+++ src/SlackMessageSender.ts
@@ -1,4 +1,6 @@
 import { WebClient, LogLevel, ChatPostMessageArguments } from '@slack/web-api';
+// @ts-ignore
+import { Log } from './db/models/index';
 
 class SlackMessageSender {
   private static instance: SlackMessageSender;
@@ -16,6 +18,47 @@ class SlackMessageSender {
     this.slackClient = new WebClient(process.env.SLACK_TOKEN, {
       logLevel: LogLevel.DEBUG,
     });
+  }
+
+  getBaseMessageTemplate(options: { title: string, log: Log }): ChatPostMessageArguments {
+    return {
+      token: process.env.SLACK_TOKEN,
+      channel: process.env.SLACK_CHANNEL_ID!,
+      text: options.log.content,
+      blocks: [
+        {
+            type: 'header',
+            text: {
+              type: 'plain_text',
+              text: `${options.title} :fire:`,
+              emoji: true
+            }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Message:*\n${options.log.content}`,
+          }
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: `*Level:*\n${options.log.level}`
+            },
+            {
+              type: 'mrkdwn',
+              text: `*Date & time:*\n${options.log.createdAt.toISOString().slice(0, 19).replace('T', ' ')}`
+            }
+          ]
+        },
+      ]
+    };
   }
 
   async publishMessage(template: ChatPostMessageArguments): Promise<boolean> {
