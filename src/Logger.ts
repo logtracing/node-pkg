ee84277bb1ce3ff6d2a06fe7789725fe1dbd9e14
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-25 10:17:30 -0600

    Integrate Slack with the basic log methods

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,12 +1,20 @@
 import { sequelize } from './db/models/index';
-import { LoggerOptions, LogModelData, LogType } from './types';
+import { GeneralOptions, LoggerOptions, LogModelData, LogType } from './types';
 // @ts-ignore
 import { Log } from './db/models/index';
 import AbstractLogger from './AbstractLogger';
+import SlackMessageSender from './SlackMessageSender';
+import { ChatPostMessageArguments } from '@slack/web-api';
 
 export default class Logger extends AbstractLogger {
-  constructor(flow: string) {
+  private slackIntegration: boolean = false;
+
+  constructor(flow: string, options?: GeneralOptions) {
     super(flow);
+
+    if (options && options.slackIntegration) {
+      this.slackIntegration = !!options.slackIntegration;
+    }
   }
 
   public async trace(content: string, opts: LoggerOptions | null = null): Promise<Log> {
@@ -16,7 +24,7 @@ export default class Logger extends AbstractLogger {
       console.error(err);
     }
   }
-  
+
   public async debug(content: string, opts: LoggerOptions | null = null): Promise<Log> {
     try {
       return await this.save(LogType.DEBUG, content, opts);
@@ -59,7 +67,7 @@ export default class Logger extends AbstractLogger {
 
   private async save(level: string, content: string, opts: LoggerOptions | null): Promise<Log> {
     const t = await sequelize.transaction();
-    
+
     try {
       const data: LogModelData = {
           level,
@@ -75,6 +83,20 @@ export default class Logger extends AbstractLogger {
         transaction: t,
       });
       await t.commit();
+
+      if (this.slackIntegration) {
+        const slackSender = SlackMessageSender.getInstance();
+        const message: ChatPostMessageArguments = slackSender.getBaseMessageTemplate({
+          title: this.flow,
+          log: log,
+        });
+
+        if (opts?.slackMessageExtraSections) {
+          message.blocks = message.blocks?.concat(opts.slackMessageExtraSections);
+        }
+
+        await slackSender.publishMessage(message);
+      }
 
       return log;
     } catch (err) {
